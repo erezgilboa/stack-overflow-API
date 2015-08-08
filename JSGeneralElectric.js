5356fc7f92c774d4a91cae295ba6a3d3fe0ed054
erezgilboa <erezgi1974@gmail.com>
2015-08-08 20:48:05 +0300

    documantation

--- JSGeneralElectric.js
+++ JSGeneralElectric.js
@@ -11,7 +11,7 @@
         $("#findid").on("click", findstackoverflow);
     }
 
-    function findstackoverflow(e)
+    function findstackoverflow(e)//when click on find ajax api calls to reputation by user
     {
         var NNAME = +$('#nNameID').val(),
             self = this;
@@ -23,11 +23,14 @@
             success: function (data)
             {
                 console.log(data);
-                
+                //clear the graph
                 $("#container").html("");
+                //the graph is for first user that get from api and represet his reputation in number
 
+                //the title
                 var names = [
 					"change_day", "change_month", "change_quarter", "change_week", "change_year"];
+                //the data
                 var chartdata = [ 
                     data.items[0].reputation_change_day,
                     data.items[0].reputation_change_month,
@@ -35,7 +38,7 @@
                     data.items[0].reputation_change_week,
                     data.items[0].reputation_change_year
                 ];
-               
+               //size of the graph and bar
                 var chart,
                  width = 1000,
                  bar_height = 20,
@@ -53,7 +56,7 @@
                    .rangeBands([0, height]);
 
          
-
+                //create graph by appending to container div 
                 var left_width = 100;
 
                 chart = d3.select($("#container")[0])
@@ -61,7 +64,7 @@
                   .attr('class', 'chart')
                   .attr('width', left_width + width)
                   .attr('height', height);
-
+                //select the data
                 chart.selectAll("rect")
                   .data(chartdata)
                   .enter().append("rect")
@@ -80,7 +83,7 @@
                   .attr("text-anchor", "end")
                   .attr('class', 'score')
                   .text(String);
-
+                //add the title to the name
                 chart.selectAll("text.name")
                   .data(names)
                   .enter().append("text")
@@ -93,7 +96,7 @@
                
 					
             },
-            error: function (message) {
+            error: function (message) {//case failed to get from server
                 $("#failureMessageNotification").html("<strong>error geting data</strong> " + message + " <a class='alert-link try-again'></a>").show()
                 
             }
